f8a01bd7f9656be1d1abd79aed98d18ae812b902
Deft Valian Exanova <deftvalian2411@gmail.com>
2025-12-26 22:40:17 +0700

    updaet

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -93,7 +93,7 @@
                     </svg>
                     Dashboard
                 </a>
-                <a href="{{ route('banners.index') }}"
+                <a href="{{ route('admin.banners.index') }}"
                     class="flex items-center p-3 text-gray-300 hover:bg-slate-800 rounded-lg">
                     <svg class="w-5 h-5 mr-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
